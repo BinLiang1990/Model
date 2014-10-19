834bfe204c7b3c7883887bcfb24ce0c369ba9e34
fireliangbin <451061218@qq.com>
2014-10-19 20:17:24 +0800

    模态层
    使用SEAJS,backbone,mustache，参考bootstrip，写的模态曾

---
+++ Scripts/App/Temp.Index.js
@@ -0,0 +1,44 @@
+﻿define(function (require) {
+    var $ = require("jquery");
+    var Common = require('common');
+
+    $(".alert").click(function () {
+        Common.Action.Alert();
+    })
+
+    $(".alert1").click(function () {
+        Common.Action.Alert({ width: 500, state: "success", title: "I'm a winner ^-^", content: "Win~~~~ ╮(￣▽￣)╭" });
+    })
+
+    $(".alert2").click(function () {
+        Common.Action.Alert({ width: 500, state: "error", title: "Error", content: "Sth is wrong" });
+    })
+
+    $(".alert3").click(function () {
+        Common.Action.Alert({ width: 500, state: "notice", title: "Pay attention class", content: "超生德V5，It's a miracle" });
+    })
+
+    $(".alert4").click(function () {
+        Common.Action.Alert({ width: 300, state: "notice", title: "又瘦了", content: "妈蛋" });
+    })
+
+    $(".alert5").click(function () {
+        Common.Action.Alert({ width: 500, title: "Block", backdrop: "default", content: "点黑的地方不隐藏了？" });
+    })
+
+    $(".alert6").click(function () {
+        Common.Action.Alert({ width: 500, title: "Shake shake shake", backdrop: "shake", content: "I'm cold" });
+    })
+
+    $(".alert7").click(function () {
+        Common.Action.Alert({ width: 500, title: "You have no idea", backdrop: "shake", content: "you have to click me", close: false });
+    })
+
+    $(".confirm").click(function () {
+        Common.Action.ShowBox(true);
+        Common.Action.Confirm().bind("confirm", function () {
+            alert("确定");
+        });
+    })
+
+})
